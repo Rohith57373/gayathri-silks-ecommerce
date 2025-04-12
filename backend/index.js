f87d22fa7632edabca34eb98856b8adb54946e0a
Rohith R <120489621+573737@users.noreply.github.com>
2025-04-12 19:52:38 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(express.json())
 app.use(cors({
-    origin: ['http://localhost:5174'],
+    origin: ['http://localhost:5173'],
     credentials: true
 }))
 
