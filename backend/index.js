84df5e7d567d4069d19eb9559f10704dd19ee2db
Rohith R <120489621+573737@users.noreply.github.com>
2025-04-12 22:45:06 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,8 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(express.json())
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', "https://gayathri-silks-ecommerce-f.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }))
 
